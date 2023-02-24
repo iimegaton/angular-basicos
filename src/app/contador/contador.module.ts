d44dced0d1c0b1903d0649b1d64ce5e19a89e5ba
Andoni Bustamante <dam32.andoni.bustamante@gmail.com>
2023-02-24 11:43:04 +0100

    primer commit

---
+++ src/app/contador/contador.module.ts
@@ -0,0 +1,18 @@
+import {NgModule} from '@angular/core'
+import { CommonModule } from "@angular/common";
+import { ContadorComponente } from './contador/contador.component';
+
+
+@NgModule({
+    declarations: [
+        ContadorComponente
+    ],
+    exports: [
+        ContadorComponente
+    ],
+    imports: [
+        CommonModule
+    ]
+
+})
+export class ContadorModule{}
